68911aa4e92e35d3dca8cf62e0df2032f0194abe
Alexandr <Formak21@ya.ru>
2022-06-09 01:11:03 +0300

    Edited License to dWTFPLv2

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
-# This project is libre, and licenced under the terms of the
-# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE, version 3.1,
-# as published by dtf on July 2019. See the COPYING file or
-# https://ph.dtf.wtf/w/wtfpl/#version-3-1 for more details.
+# This program is free software. It comes without any warranty, to
+# * the extent permitted by applicable law. You can redistribute it
+# * and/or modify it under the terms of the Do What The Fuck You Want
+# * To Public License, Version 2, as published by Sam Hocevar. See
+# * http://www.wtfpl.net/ for more details.
 
