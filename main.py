2026b84882fb38bf016ae7f3c0bc2d5ce2e1f199
Alexandr <Formak21@ya.ru>
2022-06-09 00:56:28 +0300

    License and gitignore update
    Added WTFPL instead of MIT, added some files in .gitignore

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+# This project is libre, and licenced under the terms of the
+# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENCE, version 3.1,
+# as published by dtf on July 2019. See the COPYING file or
+# https://ph.dtf.wtf/w/wtfpl/#version-3-1 for more details.
+
